92a304e22b888756633d3330b1842ad6bd30eecd
Rahul Pai <rahulpai123rockon@gmail.com>
2021-03-04 12:50:15 +0530

    Fixed grep and user filter

--- Shell/Performace/monitor.sh
+++ Shell/Performace/monitor.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
 function get_memcpu_usage() {
-    local MEMPERCENT=$(ps -o uname,pid,%mem,%cpu,bsdstart,sig,sigcatch,sigignore,sigmask,fsgid,command ax --sort -%mem,-%cpu)
-    if [[ $? -ne 0 ]]; then
-        echo "Error for Memory & CPU calculation: $MEMPERCENT"
+    if [[ -z "$1" ]]; then
+        local MEMPERCENT=$(ps -o uname,pid,%mem,%cpu,bsdstart,sig,sigcatch,sigignore,sigmask,fsgid,command ax --sort -%mem,-%cpu)
+        if [[ $? -ne 0 ]]; then
+            echo "error: $MEMPERCENT"
+            exit 1
+        fi
     else
-        echo "$MEMPERCENT | sort"
+        local MEMPERCENT=$(ps -u "$1" -o uname,pid,%mem,%cpu,bsdstart,sig,sigcatch,sigignore,sigmask,fsgid,command ax --sort -%mem,-%cpu | grep "$1")
+        if [[ $? -ne 0 ]]; then
+            echo "error: $MEMPERCENT"
+            exit 1
+        fi
     fi
+    echo "$MEMPERCENT | sort"
 }
-get_memcpu_usage
+
+if [[ $# -eq 0 ]]; then
+    get_memcpu_usage
+elif [[ $# -gt 1 ]]; then
+    echo "Enter single user name"
+    exit 1
+else
+    get_memcpu_usage $1
+fi
