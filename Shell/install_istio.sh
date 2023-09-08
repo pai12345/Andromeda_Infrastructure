489152f528447a30732311698a0629578661a6ed
pai12345 <rahulpai123@sap.com>
2023-09-08 14:54:03 +0200

    Added istio gateway and updated virtualservices

--- Shell/install_istio.sh
+++ Shell/install_istio.sh
@@ -2,7 +2,7 @@ sudo sysctl fs.protected_regular=0
 minikube addons enable metrics-server
 minikube start --force
 curl -L https://istio.io/downloadIstio | sh -
-cd istio-1.18.2
+cd istio-1.19.0
 export PATH=$PWD/bin:$PATH
 istioctl install --set profile=demo -y
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/kiali.yaml
